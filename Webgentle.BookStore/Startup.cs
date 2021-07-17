ae8a9dfeebf83dd6f2803337a68ebcdc37a94bc3
dakanisathish <dakani.sathish@gmail.com>
2021-07-17 15:06:15 +0530

    Mvc Started.

--- Webgentle.BookStore/Startup.cs
+++ Webgentle.BookStore/Startup.cs
@@ -16,6 +16,7 @@ namespace Webgentle.BookStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,15 +27,44 @@ namespace Webgentle.BookStore
                 app.UseDeveloperExceptionPage();
             }
 
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("Hello sathish Middlewar1");
+            //    await next();
+            //    await context.Response.WriteAsync("Hello sathish Middlewar1 Second");
+
+            //});
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("Hello sathish Middlewar2");
+            //    await next();
+            //    await context.Response.WriteAsync("Hello sathish Middlewar2 Second");
+            //});
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("Hello sathish Middlewar3");
+            //    await next();
+            //});
+
+
+
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
+
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/sathish", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello Sathish!");
+            //    });
+            //});
+
         }
     }
 }
